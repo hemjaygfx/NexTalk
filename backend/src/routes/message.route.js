b68d936b0fb7ada6daf2405710d6f71b6f18e7bb
hemjaygfx <hemjaystarz@gmail.com>
2026-06-30 10:22:39 +0100

    feat: implement messaging functionality with message model and routes

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,24 @@
+
 import express from 'express';
+import { 
+    getAllContacts, 
+    getChatPartners, 
+    getMessagesByUserId, 
+    sendMessage,
+   } from '../controllers/message.controller.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
+import { arcjetProtection } from '../middleware/arcjet.middleware.js';
 
     const router = express.Router();
 
-    // Message endpoint 
-router.get('/send', (req, res) => {
-    res.send('Send Message endpoint');
-}); 
+    router.use(arcjetProtection, protectRoute); // Apply Arcjet protection to all routes in this router
+
+    router.get('/contacts', getAllContacts);
+    router.get('/chats',  getChatPartners);
+    router.get('/:id', getMessagesByUserId); 
+    router.post('/send/:id', sendMessage);
 
 export default router;
+
+
+
